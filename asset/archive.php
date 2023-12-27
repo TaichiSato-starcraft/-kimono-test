b250412c002369b55bca3fd3541b11949622f990
sc_satotaichi\t-sato <t-sato@starcraft-n.co.jp>
2023-12-27 17:54:49 +0900

    コラムスタイル修正など

--- asset/archive.php
+++ asset/archive.php
@@ -1,10 +1,28 @@
 <?php get_header(); ?>
-archive-投稿一覧
+<div class="c-column">
+<h2 class="c-column__subtitle mid-txt">コラム一覧</h2>
+    <div class="c-column-box">
     <?php if ( have_posts() ) : ?>
         <?php while ( have_posts() ) : the_post(); ?>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <div class="c-column-box__item">
+                <h2 class="c-column-box__item--title"><a class="c-column__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <div class="c-column-box__item--text"><?php the_excerpt(); ?></div>
+        </div>
         <?php endwhile; ?>
     <?php else : ?>
         <p>投稿が見つかりませんでした。</p>
     <?php endif; ?>
+    </div>
+    <div class="c-column-pagination">
+        <?php
+    $args = array(
+        'mid_size' => 1,
+        'prev_text' => '&lt;&lt;前へ',
+        'next_text' => '次へ&gt;&gt;',
+        'screen_reader_text' => ' ',
+    );
+    the_posts_pagination($args);
+    ?>
+</div>
+</div>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,6 +1,20 @@
 <?php get_header();?>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <h1><?php the_title(); ?></h1>
-    <div><?php the_content(); ?></div>
-<?php endwhile; endif; ?>
+<div class="c-single">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    
+        <div class="c-block-content__white--border c-single__content">
+            <h1 class="c-single__title"><?php the_title();?></h1>
+            <div class="c-content__image">
+            <?php 
+            if ( has_post_thumbnail() ) {
+                the_post_thumbnail();
+            } else {
+                echo '<img src="' . get_stylesheet_directory_uri() . '/path/to/sample.png" alt="デフォルト画像" />';
+            }
+            ?>
+            </div>
+            <?php the_content(); ?>
+        </div>
+    <?php endwhile; endif; ?>
+</div>
 <?php get_footer();?>
